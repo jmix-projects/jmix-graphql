2e8282b87c6c97a5c8d79e98fb04e69f0e846df4
Denis Khrapov <khrapov@haulmont.com>
2021-03-23 15:24:00 +0400

    Environment for functional tests #27
    Replace generation of condition to constant

--- graphql/src/test/groovy/AbstractGraphQLTest.groovy
+++ graphql/src/test/groovy/AbstractGraphQLTest.groovy
@@ -1,19 +1,8 @@
-import graphql.Scalars
-import graphql.language.InputObjectTypeDefinition
-import graphql.language.InputValueDefinition
 import io.jmix.core.CoreConfiguration
-import io.jmix.core.MetadataTools
-import io.jmix.core.metamodel.model.MetaClass
-import io.jmix.core.metamodel.model.MetaProperty
 import io.jmix.data.DataConfiguration
 import io.jmix.eclipselink.EclipselinkConfiguration
 import io.jmix.graphql.GraphqlConfiguration
-import io.jmix.graphql.schema.FilterTypesBuilder
-import io.jmix.graphql.schema.InpTypesBuilder
-import io.jmix.graphql.schema.scalar.CustomScalars
-import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.TransactionDefinition
@@ -21,11 +10,6 @@ import org.springframework.transaction.support.TransactionTemplate
 import spock.lang.Specification
 import test_support.GraphQLTestConfiguration
 import test_support.TestContextInitializer
-
-import java.time.LocalDateTime
-import java.util.stream.Collectors
-
-import static io.jmix.graphql.schema.Types.listValueDef
 
 /*
  * Copyright 2019 Haulmont.
@@ -51,149 +35,11 @@ import static io.jmix.graphql.schema.Types.listValueDef
 )
 class AbstractGraphQLTest extends Specification {
 
-    @Autowired
-    protected JdbcTemplate jdbc
-    @Autowired
-    private InpTypesBuilder inpTypesBuilder
-    @Autowired
-    private MetadataTools metadataTools
-
     protected TransactionTemplate transaction
 
     @Autowired
     protected void setTransactionManager(PlatformTransactionManager transactionManager) {
         transaction = new TransactionTemplate(transactionManager)
         transaction.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW)
-    }
-
-    void clearTables(String... tableNames) {
-        tableNames.each {
-            clearTable(it)
-        }
-    }
-
-    protected void clearTable(String tableName) {
-        //runSqlUpdate("CREATE TABLE IF NOT EXISTS $tableName")
-        runSqlUpdate("delete from $tableName")
-    }
-
-    protected void runSqlUpdate(String sqlUpdateString) {
-        transaction.executeWithoutResult({ status ->
-            jdbc.update(sqlUpdateString)
-        })
-    }
-
-    protected InputObjectTypeDefinition buildStandardFilterType(MetaClass metaClass) {
-        String className = composeFilterConditionTypeName(metaClass)
-
-        InputObjectTypeDefinition.Builder builder = InputObjectTypeDefinition.newInputObjectDefinition()
-                .name(className)
-
-        List<InputValueDefinition> valueDefs = metaClass.getProperties().stream()
-                .map({ metaProperty ->
-
-                    if (metaProperty.getType() == MetaProperty.Type.ENUM) {
-                        return listValueDef(metaProperty.getName(), "String", null)
-                    }
-
-                    if (metaProperty.getRange().getCardinality().isMany()) {
-                        return null
-                    }
-
-                    String typeName = composeFilterConditionTypeName(getFieldTypeName(metaProperty))
-                    return listValueDef(metaProperty.getName(), typeName, null)
-                })
-                .filter(Objects.&nonNull)
-                .collect(Collectors.toList())
-
-        valueDefs.add(listValueDef(FilterTypesBuilder.ConditionUnionType.AND.name(), className, null))
-        valueDefs.add(listValueDef(FilterTypesBuilder.ConditionUnionType.OR.name(), className, null))
-
-        builder.inputValueDefinitions(valueDefs)
-
-        return builder.build()
-    }
-
-    protected static String composeFilterConditionTypeName(MetaClass metaClass) {
-        return composeFilterConditionTypeName(metaClass.getName())
-    }
-
-    protected static String composeFilterConditionTypeName(String metaClass) {
-        return composeFilterTypeName(metaClass, "FilterCondition")
-    }
-
-    protected static String composeFilterTypeName(String name, String suffix) {
-        if (!name.startsWith("inp_")) {
-            name = normalizeName(name)
-        }
-        return name + suffix
-    }
-
-    protected static String normalizeName(String entityName) {
-        return "inp_" + StringUtils.uncapitalize(entityName.replaceAll('\\$', '_'))
-    }
-
-    protected String getFieldTypeName(MetaProperty metaProperty) {
-        if (metaProperty.getType() == MetaProperty.Type.DATATYPE) {
-            return getDatatypeFieldTypeName(metaProperty)
-        }
-
-        if (metaProperty.getType() == MetaProperty.Type.ENUM) {
-            return getEnumFieldTypeName(metaProperty.getJavaType())
-        }
-
-        if ((metaProperty.getType() == MetaProperty.Type.ASSOCIATION || metaProperty.getType() == MetaProperty.Type.COMPOSITION)) {
-            return getReferenceTypeName(metaProperty)
-        }
-
-        throw new UnsupportedOperationException(String.format("Can't define field type name for metaProperty %s class %s", metaProperty, metaProperty.getJavaType()))
-    }
-
-    protected String getReferenceTypeName(MetaProperty metaProperty) {
-        if (metadataTools.isJpaEntity(metaProperty.getRange().asClass())) {
-            return normalizeName(metaProperty.getRange().asClass().getName())
-        } else {
-            return "String"
-        }
-    }
-    protected static String getEnumFieldTypeName(Class<?> javaType) {
-        return javaType.getSimpleName()
-    }
-    protected static String getDatatypeFieldTypeName(MetaProperty metaProperty) {
-        Class<?> javaType = metaProperty.getRange().asDatatype().getJavaClass()
-
-        if (String.class.isAssignableFrom(javaType))
-            return Scalars.GraphQLString.getName()
-        if (Integer.class.isAssignableFrom(javaType) || int.class.isAssignableFrom(javaType)) {
-            return Scalars.GraphQLInt.getName()
-        }
-        if (Short.class.isAssignableFrom(javaType) || short.class.isAssignableFrom(javaType)) {
-            return Scalars.GraphQLShort.getName()
-        }
-        if (Float.class.isAssignableFrom(javaType) || float.class.isAssignableFrom(javaType)
-                || Double.class.isAssignableFrom(javaType) || double.class.isAssignableFrom(javaType)) {
-            return Scalars.GraphQLFloat.getName()
-        }
-        if (Boolean.class.isAssignableFrom(javaType) || boolean.class.isAssignableFrom(javaType)) {
-            return Scalars.GraphQLBoolean.getName()
-        }
-
-        if (UUID.class.isAssignableFrom(javaType)) {
-            return CustomScalars.GraphQLUUID.getName()
-        }
-        if (Long.class.isAssignableFrom(javaType) || long.class.isAssignableFrom(javaType)) {
-            return CustomScalars.GraphQLLong.getName()
-        }
-        if (BigDecimal.class.isAssignableFrom(javaType)) {
-            return CustomScalars.GraphQLBigDecimal.getName()
-        }
-        if (Date.class.isAssignableFrom(javaType)) {
-            return CustomScalars.GraphQLDate.getName()
-        }
-        if (LocalDateTime.class.isAssignableFrom(javaType)) {
-            return CustomScalars.GraphQLLocalDateTime.getName()
-        }
-
-        return "String"
     }
 }
--- graphql/src/test/groovy/FiltrationTest.groovy
+++ graphql/src/test/groovy/FiltrationTest.groovy
@@ -1,7 +1,13 @@
+import graphql.Scalars
+import graphql.language.*
 import io.jmix.core.Metadata
 import io.jmix.graphql.schema.FilterTypesBuilder
+import io.jmix.graphql.schema.scalar.CustomScalars
+import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import test_support.entity.CarDto
+
+import javax.annotation.Nullable
 
 class FiltrationTest extends AbstractGraphQLTest {
 
@@ -21,10 +27,38 @@ class FiltrationTest extends AbstractGraphQLTest {
 
         when:
         conditionType = filterTypesBuilder.buildFilterConditionType(metadata.findClass(CarDto))
-        def standard = buildStandardFilterType(metadata.findClass(CarDto))
+        def standard = buildStandardFilterTypeCarDto()
 
         then:
         conditionType != null
         conditionType.isEqualTo(standard)
+    }
+
+    private static InputObjectTypeDefinition buildStandardFilterTypeCarDto() {
+        String className = 'inp_scr_CarDtoFilterCondition'
+
+        InputObjectTypeDefinition.Builder builder = InputObjectTypeDefinition.newInputObjectDefinition()
+                .name(className)
+
+        List<InputValueDefinition> valueDefs = new ArrayList<>()
+
+        def suffix = 'FilterCondition'
+        valueDefs.add(listValueDef("id", CustomScalars.GraphQLUUID.name + suffix, null))
+        valueDefs.add(listValueDef("manufacturer", Scalars.GraphQLString.name + suffix, null))
+        valueDefs.add(listValueDef("price", Scalars.GraphQLBigDecimal.name + suffix, null))
+        valueDefs.add(listValueDef("model", Scalars.GraphQLString.name + suffix, null))
+        valueDefs.add(listValueDef(FilterTypesBuilder.ConditionUnionType.AND.name(), className, null))
+        valueDefs.add(listValueDef(FilterTypesBuilder.ConditionUnionType.OR.name(), className, null))
+
+        builder.inputValueDefinitions(valueDefs)
+
+        return builder.build()
+    }
+
+    private static InputValueDefinition listValueDef(String fieldName, String type, @Nullable String description) {
+        return InputValueDefinition.newInputValueDefinition()
+                .name(fieldName).type(new ListType(new TypeName(type)))
+                .description(StringUtils.isBlank(description) ? null : new Description(description, null, false))
+                .build()
     }
 }
