23f11ad0e82b2c859f4b55066103c68606c6bf57
Gurylev Sergei Vitalevich <s.gurylev@haulmont.com>
2021-07-14 18:03:22 +0400

    Added possibility to upload/download files

--- graphql/src/main/java/io/jmix/graphql/controller/GraphQLFilesController.java
+++ graphql/src/main/java/io/jmix/graphql/controller/GraphQLFilesController.java
@@ -17,33 +17,60 @@
 package io.jmix.graphql.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import graphql.GraphQL;
-import groovy.util.MapEntry;
+import io.jmix.core.AccessManager;
+import io.jmix.core.FileRef;
+import io.jmix.core.FileStorage;
+import io.jmix.core.FileStorageLocator;
+import io.jmix.core.Metadata;
+import io.jmix.core.accesscontext.SpecificOperationAccessContext;
 import io.leangen.graphql.spqr.spring.web.GraphQLController;
 import io.leangen.graphql.spqr.spring.web.dto.GraphQLRequest;
 import io.leangen.graphql.spqr.spring.web.mvc.GraphQLMvcExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.annotation.Nullable;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin
-public class FilesController extends GraphQLController<NativeWebRequest> {
+public class GraphQLFilesController extends GraphQLController<NativeWebRequest> {
 
     @Autowired
-    public FilesController(GraphQL graphQL, GraphQLMvcExecutor executor) {
+    protected Metadata metadata;
+
+    @Autowired
+    protected FileStorageLocator fileStorageLocator;
+
+    @Autowired
+    protected AccessManager accessManager;
+
+    private static final Logger log = LoggerFactory.getLogger(GraphQLFilesController.class);
+
+
+    @Autowired
+    public GraphQLFilesController(GraphQL graphQL, GraphQLMvcExecutor executor) {
         super(graphQL, executor);
     }
 
@@ -56,14 +83,36 @@ public class FilesController extends GraphQLController<NativeWebRequest> {
      */
     @PostMapping(
             value = "${graphql.spqr.http.endpoint:/graphql}",
-            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.ALL_VALUE}
     )
     @ResponseBody
     public Object executeMultipartPost(@RequestPart("operations") String operations,
                                        @RequestPart("map") String map,
                                        MultipartHttpServletRequest multiPartRequest,
                                        NativeWebRequest webRequest) throws IOException, ServletException {
+
+        checkFileUploadPermission();
+
+//        FileStorage fileStorage = getFileStorage(storageName);
+//        try {
+//            String contentLength = request.getHeader("Content-Length");
+//
+//            long size = 0;
+//            try {
+//                size = Long.parseLong(contentLength);
+//            } catch (NumberFormatException ignored) {
+//            }
+//
+//            ServletInputStream is = request.getInputStream();
+//            name = Objects.toString(name, "");
+//            FileRef fileRef = uploadToFileStorage(fileStorage, is, name);
+//
+//            return createFileInfoResponseEntity(request, fileRef, name, size);
+//        } catch (Exception e) {
+//            log.error("File upload failed", e);
+//            throw new RestAPIException("File upload failed", "File upload failed", HttpStatus.INTERNAL_SERVER_ERROR, e);
+//        }
+
         GraphQLRequest graphQLRequest = new ObjectMapper().readerFor(GraphQLRequest.class).readValue(operations);
         Map<String, ArrayList<String>> fileMap = new ObjectMapper().readerFor(Map.class).readValue(map);
 
@@ -83,6 +132,39 @@ public class FilesController extends GraphQLController<NativeWebRequest> {
                 Part correspondingFile = multiPartRequest.getPart(pair.getKey());
                 graphQLRequest.getVariables().put(targetVariable, correspondingFile);
             }
+        }
+    }
+
+    protected FileStorage getFileStorage(@Nullable String storageName) {
+        if (Strings.isNullOrEmpty(storageName)) {
+            return fileStorageLocator.getDefault();
+        } else {
+            try {
+                return fileStorageLocator.getByName(storageName);
+            } catch (IllegalArgumentException e) {
+                throw new Exception("Invalid storage name",
+                        String.format("Cannot find FileStorage with the given name: '%s'", storageName),
+                        HttpStatus.BAD_REQUEST,
+                        e);
+            }
+        }
+        return null;
+    }
+
+    public class GraphQLUploadContext extends SpecificOperationAccessContext {
+        public static final String NAME = "rest.fileUpload.enabled";
+
+        public GraphQLUploadContext() {
+            super(NAME);
+        }
+    }
+
+    protected void checkFileUploadPermission() {
+        GraphQLUploadContext uploadContext = new GraphQLUploadContext();
+        accessManager.applyRegisteredConstraints(uploadContext);
+
+        if (!uploadContext.isPermitted()) {
+            throw new AccessDeniedException("File upload failed. File upload is not permitted");
         }
     }
 }
