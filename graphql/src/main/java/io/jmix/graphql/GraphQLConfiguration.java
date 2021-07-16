5a6095aa69946b34cef8f7393cf5f4cbb394ebb2
Vadim Basko <vadimbasko@users.noreply.github.com>
2021-07-16 13:48:10 +0400

    Add scalars for Date attributes annotated with TemporalType.TIMESTAMP… (#173)
    * Add scalars for Date attributes annotated with TemporalType.TIMESTAMP TemporalType.TIME #111
    
    * Add scalars for Date attributes annotated with TemporalType.TIMESTAMP TemporalType.TIME - dates #111

--- graphql/src/main/java/io/jmix/graphql/GraphQLConfiguration.java
+++ graphql/src/main/java/io/jmix/graphql/GraphQLConfiguration.java
@@ -35,6 +35,7 @@ import io.jmix.graphql.limitation.LimitationProperties;
 import io.jmix.graphql.limitation.OperationRateLimitInstrumentation;
 import io.jmix.graphql.limitation.OperationRateLimitService;
 import io.jmix.graphql.schema.*;
+import io.jmix.graphql.schema.scalar.ScalarTypes;
 import io.jmix.graphql.security.SpecificPermissionInstrumentation;
 import io.jmix.graphql.spqr.SpqrSchemaGenerator;
 import io.leangen.geantyref.GenericTypeReflector;
@@ -114,6 +115,8 @@ public class GraphQLConfiguration {
     protected Messages messages;
     @Autowired
     protected JmixTypeInfoGenerator jmixTypeInfoGenerator;
+    @Autowired
+    protected ScalarTypes scalarTypes;
 
 
     @Bean
@@ -127,10 +130,8 @@ public class GraphQLConfiguration {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
-        return builder -> {
-            builder.serializerByType(Timestamp.class,
-                    new DateSerializer(false, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")));
-        };
+        return builder -> builder.serializerByType(Timestamp.class,
+                new DateSerializer(false, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")));
     }
 
     @Bean
@@ -145,7 +146,7 @@ public class GraphQLConfiguration {
         // filter
         types.addAll(filterTypesGenerator.generateFilterTypes());
         // scalars
-        types.addAll(Arrays.asList(Types.scalars));
+        types.addAll(scalarTypes.scalars());
         // permissions
         types.addAll(permissionTypesGenerator.generatePermissionTypes());
         // messages
