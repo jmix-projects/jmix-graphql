eae8c378fe72a587d4bc8c24cfc2f42c9210cb1c
Evgeniy Bulgakov <e.bulgakov@haulmont.com>
2021-08-19 10:47:20 +0400

    Removed anonymous access annotation Haulmont/jmix-security#93

--- graphql/src/main/java/io/jmix/graphql/service/UserInfoGraphQLService.java
+++ graphql/src/main/java/io/jmix/graphql/service/UserInfoGraphQLService.java
@@ -1,7 +1,6 @@
 package io.jmix.graphql.service;
 
 import io.jmix.core.security.CurrentAuthentication;
-import io.jmix.graphql.security.GraphQLAnonymousAccess;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @GraphQLApi
-@GraphQLAnonymousAccess
 @Service("gql_UserInfoGraphQLService")
 public class UserInfoGraphQLService {
 
